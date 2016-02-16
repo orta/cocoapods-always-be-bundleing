26853b14f16aff95406cfb3c7101ed672707607c
Orta Therox <orta.therox@gmail.com>
2016-02-16 16:49:21 -0500

    v3

--- lib/cocoapods_plugin.rb
+++ lib/cocoapods_plugin.rb
@@ -9,10 +9,12 @@ module Pod
         install_before_always_be_bundling!
       else
         puts "\n\n ** Please run `pod install` with bundle exec instead. **\n\n".red
+      end
     end
 
     def validates_for_bundler
       defined?(Bundler)
     end
+    
   end
 end
